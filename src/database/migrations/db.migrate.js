545c37514878a3f450c70c781bc650b486ef2e7b
Pius <piusrestiantoro2@gmail.com>
2023-09-25 21:14:24 +0800

    Database migration created. Implemented database migration instantly by running the migrate CLI.

---
+++ src/database/migrations/db.migrate.js
@@ -0,0 +1,24 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        email VARCHAR NOT NULL,
+        password VARCHAR NOT NULL,
+        refresh_token VARCHAR,
+        token_verify VARCHAR,
+        token_expire TIMESTAMP,
+        is_verified BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      )
+    `)
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(`DROP TABLE IF EXISTS users`)
+  }
+}
